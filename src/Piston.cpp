2d8169ee142dc649edb38cb31037ed558bd759cb
menegais1 <menegais1@gmail.com>
2020-07-13 15:02:55 -0300

    Fixed piston wrong behaviour

--- src/Piston.cpp
+++ src/Piston.cpp
@@ -11,24 +11,24 @@
 
 void Piston::render() {
     Object3D::render();
-
+    dvec3 pinPosition = pin->transform.position;
+    dvec3 pinOnCrankPosition = transform.position;
     //UPPER PISTON CODE
     float radius = crank->transform.scale.y;
     float l = transform.scale.z;
     float angle = crank->transform.rotation.z * PI / 180.0;
-    pin->transform.position.y =
+    pinPosition.y =
             radius * std::cos(angle) + std::sqrt(std::pow(l, 2) - (radius * radius * std::pow(sin(angle), 2) * angle)) +
-            transform.scale.z + pin->transform.scale.z /2 -(pin->transform.scale.z /2 * 0.1) ;
+            transform.scale.z + pin->transform.scale.z /2 -(pin->transform.scale.z /2 * 0.1);
     //LOWER PISTON CODE
-    transform.position.x = crank->transform.position.x + radius * cos(angle + 90 * PI / 180);
-    transform.position.y = crank->transform.position.y + radius * sin(angle + 90 * PI / 180) + transform.scale.z / 2.0;
-    
-    dvec2 vec = pin->transform.position - transform.position;
+    pinOnCrankPosition.x = crank->transform.position.x + radius * cos(angle + 90 * PI / 180);
+    pinOnCrankPosition.y = crank->transform.position.y + radius * sin(angle + 90 * PI / 180) ;
+    transform.position = ((pinPosition + pinOnCrankPosition) / 2);
+    transform.position.y -=pin->transform.scale.z / 4;
+    dvec2 vec = pinPosition - pinOnCrankPosition;
     angle = std::atan2(vec.y, vec.x);
     transform.rotation.z = angle * 180.0 / PI + 90;
-    float deslocationInX = transform.scale.x;
-    transform.position.x =
-            (crank->transform.position.x + deslocationInX) + deslocationInX * cos(transform.rotation.z * (PI / 180.0));
+    pin->transform.position = pinPosition;
 }
 
 Piston::Piston(Transform transform) : Cylinder(transform) {
